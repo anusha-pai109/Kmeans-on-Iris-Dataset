780a9701582a840629ec4c65d1fb5a3baf2d94f7
anusha-pai109 <64679339+anusha-pai109@users.noreply.github.com>
2021-06-01 15:39:08 +0530

    Add files via upload

---
+++ kmeansiris.r
@@ -0,0 +1,31 @@
+require("datasets")
+data(iris)
+View(iris)
+str(iris)
+summary(iris)
+head(iris)
+iris.new<- iris[,c(1,2,3,4)]
+iris.class<- iris[,"Species"]
+head(iris.new)
+head(iris.class)
+normalize <- function(x){
+  return ((x-min(x))/(max(x)-min(x)))
+}
+
+iris.new$Sepal.Length<- normalize(iris.new$Sepal.Length)
+iris.new$Sepal.Width<- normalize(iris.new$Sepal.Width)
+iris.new$Petal.Length<- normalize(iris.new$Petal.Length)
+iris.new$Petal.Width<- normalize(iris.new$Petal.Width)
+head(iris.new)
+
+result <- kmeans(iris.new,3)
+result$size
+result$centers
+result$cluster
+par(mfrow=c(2,2), mar=c(5,4,2,2))
+plot(iris.new[c(1,2)], col=result$cluster)# Plot to see how Sepal.Length and Sepal.Width data points have been distributed in clusters
+plot(iris.new[c(1,2)], col=iris.class)# Plot to see how Sepal.Length and Sepal.Width data points have been distributed originally as per "class" attribute in dataset
+plot(iris.new[c(3,4)], col=result$cluster)# Plot to see how Petal.Length and Petal.Width data points have been distributed in clusters
+plot(iris.new[c(3,4)], col=iris.class)
+
+table(result$cluster,iris.class)
